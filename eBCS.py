4f867a67a8c23794bbd7bed93ccb7f07ec2ead88
hughec1329 <hughcrockford@gmail.com>
2013-03-22 13:53:23 -0700

    post spell

--- eBCS.py
+++ eBCS.py
@@ -14,10 +14,7 @@ def get_imlist(path,ext):
 	#  Returns a list of filenames for all jpg images in a directory.
         return [os.path.join(path,f) for f in os.listdir(path) if f.endswith(ext)]
 
-
 cow = array(Image.open("./cows/elanco/200.jpg"))
-cv.
-
 
 ## Canny edge detecting
 
@@ -31,9 +28,8 @@ imshow(edges1)
 moo = cv.LoadImage("./cows/elanco/200.jpg")
 stor = cvCreateMemStorage(0)
 cv.FindContours(cv.fromarray(daisy),storself.# nothing?
-cv.Dra
 
-## should use cv2
+## should use cv2 - needed to cmake - compile
 import cv2
 cvin = imread(".cows/elanco/200.jpg")
 # cvin2 = cv2.cvtColor(cvin, cv2.COLOR_BGR2GRAY) 			# convert color to bgay - not working?
@@ -43,13 +39,11 @@ descriptor = cv2.DescriptorExtractor_create("SIFT") 		# build feature extractor.
 dt = detector.detect(cvin)
 imshow(cv2.drawKeypoints(cvin,dt))  	# SIFT seems to get better kp.
 
-
-## WROKING - return dt.pt, .size, .angle - all things to model on? can determine which important using PCA? need to find same features in each.
+## WORKING - return dt.pt, .size, .angle - all things to model on? can determine which important using PCA? need to find same features in each.
 
 grid_d = cv2.GridAdaptedFeatureDetector(detector,100) 		# extract 100 features
 
 imshow(cv2.drawKeypoints(cvin,grid_d.detect(cvin))  		# grid holds to 100 - getting them up in head region  - crop on way in?
-
 
 cv2.DescriptorMatcher_create(
 
@@ -58,7 +52,6 @@ wave = cv2.SURF(400)
 y = wave.detect(cvin) 		# get keypoints
 
 imshow(cv2.drawKeypoints(cvin,y)) 	# draw them
-
 
 # moments
 cv2.moments(daisy)
@@ -72,13 +65,14 @@ from skimage import measure
 moo = cv.LoadImage("./cows/elanco/200.jpg")
 cont = measure.find_contours(moo,0.8)
 
+daisy=imread("./cows/elanco/200.jpg")
+cv.Flip(cv.fromarray(daisy))
+daisygrey = cv2.cvtColor(daisy,cv2.COLOR_RGB2GRAY)
+
 daisy_cont = cv2.findContours(daisy,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE) 	# need a black background w white cow - threshold?Z
 cv2.drawContours(daisy,i,-1,(0,255,0),3)
 
-
-######
 # for writeup
-######
 import matplotlib.pyplot as plt
 
 cow = imread("./cows/real/25.png")
@@ -94,8 +88,7 @@ plt.hist(blackcow)
 
 imshow(blackcow,cmap = cm.Greys_r)					# show blackcow
 
-
-
-
+daisy_cont = cv2.findContours(blurgrey,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE) 	# need a black background w white cow - threshold?Z
+cv2.drawContours(daisy,i,-1,(0,255,0),3)
 
 imshow(cv2.cvtColor(blurcow,cv2.COLOR_HSV2GRAY))
